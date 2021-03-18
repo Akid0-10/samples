54ccd674b9df6a5d9af64597a96c138f101f1fa0
Akid0-10 <n.akid89@gmail.com>
2021-03-18 23:15:43 +0800

    Add more attributes to Location.java

--- samples/src/samples/demo/Location.java
+++ samples/src/samples/demo/Location.java
@@ -3,12 +3,30 @@ package samples.demo;
 public class Location {
 
 	private String locationName;
+	private int locationID;
+	private String locationDetail;
 	
 	public void setLocationName(String locationName) {
 		this.locationName=locationName;
 	}
 	
+	public void setLocationID(int locationID) {
+		this.locationID=locationID;
+	}
+	
+	public void setLocationDetail(String locationDetail) {
+		this.locationDetail=locationDetail;
+	}
+	
 	public String getLocationName() {
 		return locationName;
+	}
+	
+	public int getLocationID() {
+		return locationID;
+	}
+	
+	public String getLocationDetail() {
+		return locationDetail;
 	}
 }
