eb8c42b9ac094562edf02e8ac50a147cf26202c6
Akid0-10 <n.akid89@gmail.com>
2021-03-18 23:10:40 +0800

    Initial commit of Location.java

---
+++ samples/src/samples/demo/Location.java
@@ -0,0 +1,14 @@
+package samples.demo;
+
+public class Location {
+
+	private String locationName;
+	
+	public void setLocationName(String locationName) {
+		this.locationName=locationName;
+	}
+	
+	public String getLocationName() {
+		return locationName;
+	}
+}
