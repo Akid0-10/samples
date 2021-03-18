26d610eb796df34eb92f9e240a02396f97bd4c8d
Akid0-10 <n.akid89@gmail.com>
2021-03-18 23:16:25 +0800

    Modify Location.java

--- samples/src/samples/demo/Location.java
+++ samples/src/samples/demo/Location.java
@@ -4,7 +4,6 @@ public class Location {
 
 	private String locationName;
 	private int locationID;
-	private String locationDetail;
 	
 	public void setLocationName(String locationName) {
 		this.locationName=locationName;
@@ -12,10 +11,6 @@ public class Location {
 	
 	public void setLocationID(int locationID) {
 		this.locationID=locationID;
-	}
-	
-	public void setLocationDetail(String locationDetail) {
-		this.locationDetail=locationDetail;
 	}
 	
 	public String getLocationName() {
@@ -26,7 +21,4 @@ public class Location {
 		return locationID;
 	}
 	
-	public String getLocationDetail() {
-		return locationDetail;
-	}
 }
